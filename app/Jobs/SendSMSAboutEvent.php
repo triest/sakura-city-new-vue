ab5abe74dbf5871fa47bc0769275a874352aab04
triest <triest21@gmail.com>
2019-07-03 22:01:12 +0300

    add event for send sms and mail

--- app/Jobs/SendSMSAboutEvent.php
+++ app/Jobs/SendSMSAboutEvent.php
@@ -40,16 +40,38 @@ class SendSMSAboutEvent implements ShouldQueue
     public function handle()
     {
         //
-        $name = $this->name;
-        $mail2 = $this->mail;
-        $event = $this->event;
-        Mail::send('mail.test', ['name' => $name, 'event' => $event], function ($message) use ($mail2) {
-            $message
-                ->to($mail2, 'some guy')
-                ->from('sakura-testmail@sakura-city.info')
-                ->subject('Обновление ваших событий!');
-        });
-
-        return null;
+        $src = '<?xml version="1.0" encoding="UTF-8"?>
+        <SMS>
+            <operations>
+            <operation>SEND</operation>
+            </operations>
+            <authentification>
+            <username>sakura-city@rambler.ru</username>
+            <password>22d2af28</password>
+            </authentification>
+            <message>
+            <sender>SMS</sender>
+            <text>'.$this->message.'</text>
+            </message>
+            <numbers>
+            <number messageID="msg11">'.$this->phone.'</number>
+            </numbers>
+        </SMS>';
+        $Curl = curl_init();
+        $CurlOptions = array(
+            CURLOPT_URL => 'http://api.atompark.com/members/sms/xml.php',
+            CURLOPT_FOLLOWLOCATION => false,
+            CURLOPT_POST => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CONNECTTIMEOUT => 15,
+            CURLOPT_TIMEOUT => 100,
+            CURLOPT_POSTFIELDS => array('XML' => $src),
+        );
+        curl_setopt_array($Curl, $CurlOptions);
+        if (false === ($Result = curl_exec($Curl))) {
+            throw new Exception('Http request failed');
+        }
+        curl_close($Curl);
     }
 }
